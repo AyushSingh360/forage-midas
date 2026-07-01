5af6b496f42dcc2dd91b25d2fd9927fe120e0065
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-01 09:01:02 +0530

    feat: implement transaction consumer to process Kafka messages and calculate incentives via external service

--- src/main/java/com/jpmc/midascore/component/TransactionConsumer.java
+++ src/main/java/com/jpmc/midascore/component/TransactionConsumer.java
@@ -2,6 +2,7 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class TransactionConsumer {
@@ -17,10 +19,12 @@ public class TransactionConsumer {
 
     private final TransactionRepository transactionRepository;
     private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
 
-    public TransactionConsumer(TransactionRepository transactionRepository, UserRepository userRepository) {
+    public TransactionConsumer(TransactionRepository transactionRepository, UserRepository userRepository, RestTemplate restTemplate) {
         this.transactionRepository = transactionRepository;
         this.userRepository = userRepository;
+        this.restTemplate = restTemplate;
     }
 
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
@@ -40,13 +44,17 @@ public class TransactionConsumer {
             return;
         }
 
+        Incentive incentive = restTemplate.postForObject("http://localhost:8080/incentive", transaction, Incentive.class);
+        float incentiveAmount = incentive != null ? incentive.getAmount() : 0f;
+        logger.info("Incentive received: {}", incentiveAmount);
+
         sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
 
         userRepository.save(sender);
         userRepository.save(recipient);
 
-        TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount());
+        TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount);
         transactionRepository.save(record);
 
         logger.info("Transaction processed successfully: {}", record);
